bf8dcd12a5c24af744a36c1d43d0678674cecc75
tkdn <tkdnation@gmail.com>
2026-07-23 19:27:23 +0900

    chore: my1weekpr

--- private_dot_config/private_fish/functions/mypr1week.fish
+++ private_dot_config/private_fish/functions/mypr1week.fish
@@ -1,12 +1,32 @@
 function mypr1week
-  gh search prs \
-    --author tkdn --closed '>'(date -v -7d +%Y-%m-%d) \
-    --json repository,number,title,closedAt,url \
-    --jq '.[] | [
+  set -l days 7
+  set -l user tkdn
+
+  for arg in $argv
+    switch $arg
+      case '-d=*'
+        set days (string match -r -- '-d=(\d+)' $arg)[2]
+      case '-u=*'
+        set user (string match -r -- '-u=(.+)' $arg)[2]
+    end
+  end
+
+  set -l since (date -v -{$days}d +%Y-%m-%d)
+  set -l jq_filter '.[] | [
       .repository.nameWithOwner,
       .number,
       (.closedAt | sub("Z$"; "") | strptime("%Y-%m-%dT%H:%M:%S") | mktime | . + (9*3600) | strftime("%Y-%m-%d %H:%M:%S")),
       .title,
       .url
     ] | @tsv'
+
+  echo "=== authored ==="
+  gh search prs --author $user --closed ">$since" \
+    --json repository,number,title,closedAt,url \
+    --jq $jq_filter
+
+  echo "=== reviewed ==="
+  gh search prs --reviewed-by $user --closed ">$since" \
+    --json repository,number,title,closedAt,url \
+    --jq $jq_filter
 end
